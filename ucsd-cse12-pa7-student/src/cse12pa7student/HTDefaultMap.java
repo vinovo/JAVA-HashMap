033e577f00f3b6670ed2ed71cc255e0db8ba9f7e
vino <vino@128.54.181.189>
2018-03-03 22:28:50 -0800

    2018-3-3 22:28:42 All implemented

--- ucsd-cse12-pa7-student/src/cse12pa7student/HTDefaultMap.java
+++ ucsd-cse12-pa7-student/src/cse12pa7student/HTDefaultMap.java
@@ -1,65 +1,184 @@
 package cse12pa7student;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
-public class HTDefaultMap<K,V> implements DefaultMap<K,V> {
+public class HTDefaultMap<K, V> implements DefaultMap<K, V> {
 
 	/* Fields */
-	private List<Pair<K,V>>[] buckets;
+	private List<Pair<K, V>>[] buckets;
+	private V defaultValue;
+	private double loadThreshold;
+	private int capacity, size, expansionFactor;
+	private Hasher<K> hasher;
 
 	/* Constructor */
-	public HTDefaultMap(V defaultValue, int startCapacity, double loadThreshold, int expansionFactor, Hasher<K> hasher){
+	public HTDefaultMap(V defaultValue, int startCapacity, double loadThreshold, int expansionFactor,
+			Hasher<K> hasher) {
+		if (startCapacity <= 0)
+			startCapacity = 1;
+		this.defaultValue = defaultValue;
+		this.capacity = startCapacity;
+		this.loadThreshold = loadThreshold;
+		this.expansionFactor = expansionFactor;
+		this.hasher = hasher;
+		this.size = 0;
+		buckets = new List[startCapacity];
 	}
-		
+
 	/* Fill in these methods */
 	@Override
 	public void set(K key, V value) {
+		if (key == null)
+			throw new IllegalArgumentException("key is null");
+		else {
+			Pair<K, V> newPair = new Pair<K, V>(key, value);
+			int hashCode = hashToBuckets(key);
+			if (buckets[hashCode] == null) {
+				buckets[hashCode] = new ArrayList<Pair<K, V>>();
+				buckets[hashCode].add(newPair);
+			} else {
+				boolean found = false;
+				for (int i = 0; i < buckets[hashCode].size(); i++) {
+					// if the key already exists.
+					if (buckets[hashCode].get(i).key.equals(key)) {
+						buckets[hashCode].set(i, newPair);
+						found = true;
+						this.size--;
+					}
+				}
+				if (!found)
+					buckets[hashCode].add(newPair);
+			}
+			this.size++;
+			if (currentLoadFactor() >= loadThreshold)
+				rehash();
+		}
 	}
 
 	@Override
 	public V get(K key) {
-		return null;
+		if (key == null)
+			throw new IllegalArgumentException("key is null");
+		else {
+			int hashCode = hashToBuckets(key);
+			if (buckets[hashCode] != null) {
+				for (Pair<K, V> p : buckets[hashCode]) {
+					if (p.key.equals(key)){
+						return p.value;
+					}
+				}
+			}
+			if (this.defaultValue == null)
+				throw new NoSuchElementException("key not found!");
+			else 
+				return this.defaultValue;
+		}
 	}
 
 	@Override
 	public boolean containsKey(K key) {
+		if (key == null)
+			return false;
+		int hashCode = hashToBuckets(key);
+		if (buckets[hashCode] == null)
+			return false;
+		for (Pair<K,V> p : buckets[hashCode]){
+			if (p.key.equals(key))
+				return true;
+		}
 		return false;
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return this.size;
 	}
 
 	@Override
 	public V defaultValue() {
-		return null;
+		return this.defaultValue;
 	}
 
 	@Override
 	public List<K> keys() {
-		return null;
+		List<K> keyList = new ArrayList<K>();
+		for (List<Pair<K,V>> lst : buckets){
+			if (lst != null){
+				for (Pair<K,V> p : lst){
+					keyList.add(p.key);
+				}
+			}
+		}
+		return keyList;
 	}
 
 	@Override
 	public List<V> values() {
-		return null;
+		List<V> valueList = new ArrayList<V>();
+		for (List<Pair<K,V>> lst : buckets){
+			if (lst != null){
+				for (Pair<K,V> p : lst){
+					valueList.add(p.value);
+				}
+			}
+		}
+		return valueList;
 	}
-	
+
 	public int totalCollisions() {
-		return 0;
+		int collisions = 0;
+		for (List<Pair<K,V>> lst : buckets){
+			if (lst != null && lst.size() > 0)
+				collisions += lst.size() - 1;
+		}
+		return collisions;
 	}
-	
+
 	public int currentCapacity() {
-		return 0;
+		return this.capacity;
 	}
-	
+
 	public double currentLoadFactor() {
-		return 0.0;
+		return (double) this.size / this.capacity;
 	}
-	
+
 	public int deepestChain() {
-		return 0;
+		int maxDepth = 0;
+		for (List<Pair<K,V>> lst : buckets){
+			if (lst != null && lst.size() > maxDepth){
+				maxDepth = lst.size();
+			}
+		}
+		return maxDepth;
 	}
-	
+
+	// Helper method that hash key to bucket.
+	private int hashToBuckets(K key) {
+		int hashCode;
+		if (hasher == null)
+			hashCode = key.hashCode();
+		else
+			hashCode = hasher.hash(key);
+		return hashCode % capacity;
+	}
+
+	// Helper method to rehash
+	private void rehash() {
+		this.capacity = this.capacity * this.expansionFactor;
+		List<Pair<K, V>>[] newBuckets = new List[this.capacity];
+		for (List<Pair<K, V>> b : this.buckets) {
+			if (b != null) {
+				for (Pair<K, V> p : b) {
+					int hashCode = hashToBuckets(p.key);
+					if (newBuckets[hashCode] == null)
+						newBuckets[hashCode] = new ArrayList<Pair<K, V>>();
+					newBuckets[hashCode].add(p);
+				}
+			}
+		}
+		buckets = newBuckets;
+	}
+
 }
--- ucsd-cse12-pa7-student/src/cse12pa7student/TestHTDefaultMap.java
+++ ucsd-cse12-pa7-student/src/cse12pa7student/TestHTDefaultMap.java
@@ -1,10 +1,198 @@
 package cse12pa7student;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.junit.*;
 
 public class TestHTDefaultMap {
 
-	//** Put your JUnit tests here **// 
+	class customHasher<K> implements Hasher<K> {
+		@Override
+		public int hash(K k) {
+			String a = k.toString();
+			return a.length();
+		}
+
+	}
+
+	Hasher<String> hasher = new customHasher<String>();
+
+	// ** Put your JUnit tests here **//
+	@Test
+	public void testSetAndGet() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(10000, 1, 0.75, 2, hasher);
+		ht.set("red", 281);
+		assertEquals((Integer) ht.get("red"), (Integer) 281);
+		ht.set("", 0);
+		assertEquals((Integer) ht.get(""), (Integer) 0);
+		ht.set("blue", 322);
+		assertEquals((Integer) ht.get("blue"), (Integer) 322);
+		ht.set("teal", 99);
+		assertEquals((Integer) ht.get("teal"), (Integer) 99);
+		ht.set("red", 81);
+		assertEquals((Integer) ht.get("red"), (Integer) 81);
+	}
+
+	@Test
+	public void getNull() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(0, 5, 1, 2, hasher);
+		ht.set("A", null);
+		assertNull(ht.get("A"));
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void nullDefaultValue() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 5, 0.1, 5, null);
+		ht.get("A");
+	}
+
+	@Test
+	public void testContainsKey() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 0.7, 3, null);
+		assertFalse(ht.containsKey(""));
+		for (int i = 65; i < 91; i++) {
+			ht.set((char) i + "", i);
+		}
+		assertTrue(ht.containsKey("Z"));
+		ht.set("Z", 65538);
+		assertTrue(ht.containsKey("Z"));
+		assertTrue(ht.containsKey("H"));
+		assertFalse(ht.containsKey("!@#$%^&(*)"));
+	}
+
+	@Test
+	public void testSize() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 0.7, 3, null);
+		assertEquals(0, ht.size());
+		for (int i = 65; i < 91; i++) {
+			ht.set((char) i + "", i);
+		}
+		assertEquals(26, ht.size());
+		ht.set("a", 1);
+		ht.set("Z", 26); // should not change the size
+		assertEquals(27, ht.size());
+	}
+
+	@Test
+	public void testDefaultValue() {
+		HTDefaultMap<String, Integer> ht1 = new HTDefaultMap<String, Integer>(null, 4, 0.7, 3, null);
+		assertNull(ht1.defaultValue());
+		HTDefaultMap<String, Integer> ht2 = new HTDefaultMap<String, Integer>(0, 4, 0.7, 3, null);
+		assertEquals((Integer) 0, (Integer) ht2.defaultValue());
+		HTDefaultMap<String, Integer> ht3 = new HTDefaultMap<String, Integer>(1000000, 4, 0.7, 3, null);
+		assertEquals((Integer) 1000000, (Integer) ht3.defaultValue());
+	}
+
+	@Test
+	public void testKeys() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 0.7, 3, null);
+		for (int i = 65; i < 91; i++) {
+			ht.set((char) i + "", i);
+		}
+		List<String> lst = ht.keys();
+		assertEquals(ht.size(), lst.size());
+		for (int i = 65; i < 91; i++) {
+			assertTrue(lst.contains((char) i + ""));
+		}
+		assertFalse(lst.contains("anythingFancy"));
+	}
+
+	@Test
+	public void testValues() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 0.7, 3, null);
+		for (int i = 65; i < 91; i++) {
+			ht.set((char) i + "", i);
+		}
+		List<Integer> lst = ht.values();
+		assertEquals(ht.size(), lst.size());
+		for (int i = 65; i < 91; i++) {
+			assertTrue(lst.contains((i)));
+		}
+		assertFalse(lst.contains(100000));
+	}
+
+	@Test
+	public void testTotalCollisions() {
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 1, 2, hasher);
+		ht.set("aa", 2);
+		ht.set("abcdef", 4);
+		assertEquals(1, ht.totalCollisions());
+		ht.set("ad", 2);
+		assertEquals(2, ht.totalCollisions());
+		ht.set("a", 1);
+		// capacity is now 8, "abcdef" no longer collides with Strings of length
+		// 2
+		assertEquals(1, ht.totalCollisions());
+		ht.set("a", 100);
+		ht.set("b", 1);
+		assertEquals(2, ht.totalCollisions());
+	}
+
+	@Test
+	public void currentCapacity() {
+		HTDefaultMap<String, Integer> ht1 = new HTDefaultMap<String, Integer>(null, 1, 0.7, 3, null);
+		assertEquals(1, ht1.currentCapacity());
+		ht1.set("a", 1);
+		assertEquals(3, ht1.currentCapacity());
+		ht1.set("b", 2);
+		assertEquals(3, ht1.currentCapacity());
+		ht1.set("b", 222);
+		// loadFactor is 2/3, less than 0.7
+		assertEquals(3, ht1.currentCapacity());
+		ht1.set("c", 3);
+		assertEquals(9, ht1.currentCapacity());
+		ht1.set("d", 4);
+		ht1.set("e", 5);
+		ht1.set("f", 6);
+		// current LoadFactor is 6/9, less than 0.7
+		assertEquals(9, ht1.currentCapacity());
+		ht1.set("g", 7);
+		assertEquals(27, ht1.currentCapacity());
+		HTDefaultMap<String, Integer> ht2 = new HTDefaultMap<String, Integer>(null, 2, 1, 2, null);
+		assertEquals(2, ht2.currentCapacity());
+		ht2.set("a", 1);
+		ht2.set("b", 2);
+		assertEquals(4, ht2.currentCapacity());
+		ht2.set("c", 3);
+		ht2.set("d", 4);
+		assertEquals(8, ht2.currentCapacity());
+		for (int i = 65; i < 91; i++) {
+			ht2.set((char) i + "", i);
+		}
+		// At this line there should be 30 keys in the map. The capacity should
+		// have been updated to 16*2
+		assertEquals(32, ht2.currentCapacity());
+		ht2.set("This PA", 31);
+		ht2.set("is hard", 32);
+		assertEquals(64, ht2.currentCapacity());
+	}
 	
+	@Test
+	public void testCurrentLoadFactor(){
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 1, 2, hasher);
+		assertEquals(0.0, ht.currentLoadFactor(), 0);
+		ht.set("a", 1);
+		ht.set("ab", 2);
+		assertEquals(0.5, ht.currentLoadFactor(), 0);
+		ht.set("abc", 3);
+		assertEquals(0.75, ht.currentLoadFactor(), 0);
+		ht.set("abcd", 4);
+		assertEquals(0.5, ht.currentLoadFactor(), 0);
+	}
+	
+	@Test
+	public void testDeepestChain(){
+		HTDefaultMap<String, Integer> ht = new HTDefaultMap<String, Integer>(null, 4, 1, 2, hasher);
+		ht.set("somekey", 7);
+		ht.set("abc", 3);
+		ht.set("abcd", 4);
+		assertEquals(2, ht.deepestChain());
+		for (int i = 65; i < 91; i++) {
+			ht.set((char) i + "", i);
+		}
+		assertEquals(26, ht.deepestChain());
+	}
 }
